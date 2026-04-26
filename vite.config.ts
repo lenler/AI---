d26c8620ca01ca93dad49e90df611014dc85976d
bule_punkin <2122151459@qq.com>
2026-04-26 21:21:53 +0800

    白屏问题解答

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { traeBadgePlugin } from "vite-plugin-trae-solo-badge";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/<repo>/",
+  base: "./",
   server: {
     watch: {
       usePolling: true,
